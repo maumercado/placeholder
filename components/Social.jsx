a3a71701f27cad86b61564390bf4d93bdfeb5b1e
Mauricio Mercado <mau@maumercado.com>
2019-06-12 07:33:54 -0700

    color change to icons

--- components/Social.jsx
+++ components/Social.jsx
@@ -10,18 +10,18 @@ import {
 export default function SocialFollow() {
   return (
       <React.Fragment>
-        <a href={'https://www.twitter.com/maumercado'} style={{ margin: '0 5px' }} className={'twitter social'}>
+        <a href={'https://www.twitter.com/maumercado'} style={{ margin: '0 5px', color: 'white' }} className={'twitter social'}>
           <FontAwesomeIcon icon={faTwitter} size="2x" />
           </a>
-        <a href={'https://www.github.com/maumercado'} style={{ margin: '0 5px' }} className={'github social'}>
+        <a href={'https://www.github.com/maumercado'} style={{ margin: '0 5px', color: 'white' }} className={'github social'}>
             <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
 
-      <a href={'https://www.instagram.com/maumercado'} style={{ margin: '0 5px' }} className={'instagram social'}>
+      <a href={'https://www.instagram.com/maumercado'} style={{ margin: '0 5px', color: 'white' }} className={'instagram social'}>
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
 
-      <a href={'https://www.linkedin.com/in/mauromercado/'} style={{ margin: '0 5px' }} className={'linkedin social'}>
+      <a href={'https://www.linkedin.com/in/mauromercado/'} style={{ margin: '0 5px', color: 'white' }} className={'linkedin social'}>
         <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
       </a>
       </React.Fragment>
