4423f9f7be8ebf139971c5f7ccf2a99fb4124701
Mauricio Mercado <mau@maumercado.com>
2019-06-12 00:10:31 -0700

    Add deck

---
+++ components/Social.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faInstagram,
+  faGithub,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+
+export default function SocialFollow() {
+  return (
+      <React.Fragment>
+        <a href={'https://www.twitter.com/maumercado'} style={{ margin: '0 5px' }} className={'twitter social'}>
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+        <a href={'https://www.github.com/maumercado'} style={{ margin: '0 5px' }} className={'github social'}>
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
+
+      <a href={'https://www.instagram.com/maumercado'} style={{ margin: '0 5px' }} className={'instagram social'}>
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+
+      <a href={'https://www.linkedin.com/in/mauromercado/'} style={{ margin: '0 5px' }} className={'linkedin social'}>
+        <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+      </a>
+      </React.Fragment>
+  );
+}
