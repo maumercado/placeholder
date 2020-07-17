c87eb09c972f68d6d26c97a8a6bb04cddb4dfb63
Mauricio Mercado <mauricio.mercado@telus.com>
2020-07-17 03:21:28 +0000

    add blog

--- components/Social.jsx
+++ components/Social.jsx
@@ -4,8 +4,10 @@ import {
   faTwitter,
   faInstagram,
   faGithub,
-  faLinkedinIn,
+  faLinkedinIn
 } from '@fortawesome/free-brands-svg-icons';
+import { faBlog } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function SocialFollow() {
   return (
@@ -13,17 +15,20 @@ export default function SocialFollow() {
         <a href={'https://www.twitter.com/maumercado'} style={{ margin: '0 5px', color: 'white' }} className={'twitter social'}>
           <FontAwesomeIcon icon={faTwitter} size="2x" />
           </a>
-        <a href={'https://www.github.com/maumercado'} style={{ margin: '0 5px', color: 'white' }} className={'github social'}>
+        <a href={'https://www.github.com/maumercado'} style={{ margin: '0 7px', color: 'white' }} className={'github social'}>
             <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
 
-      <a href={'https://www.instagram.com/maumercado'} style={{ margin: '0 5px', color: 'white' }} className={'instagram social'}>
+      <a href={'https://www.instagram.com/maumercado'} style={{ margin: '0 7px', color: 'white' }} className={'instagram social'}>
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
 
-      <a href={'https://www.linkedin.com/in/mauromercado/'} style={{ margin: '0 5px', color: 'white' }} className={'linkedin social'}>
+      <a href={'https://www.linkedin.com/in/mauromercado/'} style={{ margin: '0 7px', color: 'white' }} className={'linkedin social'}>
         <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
       </a>
+      <a href={'https://codigo.sh/'} style={{ margin: '0 7px', color: 'white' }} className={'blog social'}>
+        <FontAwesomeIcon icon={faBlog} size="2x" />
+     </a>
       </React.Fragment>
   );
 }
